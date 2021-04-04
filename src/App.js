697d97a1f26c6cc2903a2425bbea1ae0744841b8
Jitendra Kumar <jitendrar048@gmail.com>
2021-04-04 15:35:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      <h1>Hey Jitendra... Let's build a twitter clone!</h1>
+    </div>
+  );
+}
+
+export default App;
